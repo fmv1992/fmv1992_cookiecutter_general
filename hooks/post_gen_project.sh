3e1fe8451b5fe9538caf2107386966954b152ad0
Felipe M. Vieira <felipev@telnyx.com>
2024-10-22 20:06:13 -0300

    updated data

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -16,4 +16,12 @@ bname="$(basename "$0")"
 
 echo "Running \`${bname}\` in \`${PWD}\`." >&2
 
+find . -type f -print0 \
+    | parallel --null --group --keep-order --jobs $(nproc) --max-args 10 \
+        -- \
+        '
+        sed --in-place --regexp-extended '"'"'s#⟨⟨⟨#{{{#g'"'"' {}
+        sed --in-place --regexp-extended '"'"'s#⟩⟩⟩#}}}#g'"'"' {}
+        '
+
 # vim: set filetype=sh fileformat=unix nowrap:
